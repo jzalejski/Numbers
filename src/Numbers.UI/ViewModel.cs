f68899c9ca8dcaea4797b576756a80ee3307c3d3
zalejskij <zalejskij@ryanair.com>
2017-03-23 17:55:15 +0100

    init commit

---
+++ src/Numbers.UI/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace Numbers.UI
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private readonly IController controller;
+        private string userInput;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel(IController controller)
+        {
+            this.controller = controller;
+        }
+
+        public string UserInput
+        {
+            get { return userInput; }
+            set
+            {
+                if (value != userInput)
+                {
+                    userInput = value;
+                    OnPropertyChanged(nameof(UserInput));
+                }
+                
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public interface IController
+    {
+        string Convert(string userInput);
+    }
+}
