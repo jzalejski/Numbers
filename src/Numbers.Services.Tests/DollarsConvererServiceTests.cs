23557b1e44c92ef501b501db7e5d58be493c6700
jzzi <janusz.zalejski+github@gmail.com>
2017-03-26 14:28:44 +0200

    ServiceTests

---
+++ src/Numbers.Services.Tests/DollarsConvererServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Numbers.Services.Tests
+{
+    [TestFixture]
+    public class DollarsConvererServiceTests
+    {
+        [Test]
+        public void ShouldSetResultIfParsingValidMessage()
+        {
+            var mock = MockRepository.GenerateMock<INumbersConverter>();
+            var returnedValue = "test result";
+            mock.Stub(p => p.Convert(Arg<string>.Is.Anything)).Return(returnedValue);
+            var service = new DollarsConverterService(mock);
+            var result = service.Convert("12,21");
+            Assert.That(result.Words, Is.EqualTo(returnedValue));
+            Assert.That(result.Error, Is.Null);
+        }
+
+        [Test]
+        public void ShouldSetErrorIfParsingInvalidMessage()
+        {
+            var mock = MockRepository.GenerateMock<INumbersConverter>();
+            var input = "bad input";
+            var excetpionThrown = new InputInvalidFormatExcetpion(input);
+            mock.Stub(p => p.Convert(Arg<string>.Is.Anything)).Throw(excetpionThrown);
+            var service = new DollarsConverterService(mock);
+            
+            var result = service.Convert(input);
+            Assert.That(result.Error, Is.EqualTo(excetpionThrown.Message));
+            Assert.That(result.Words, Is.Null);
+        }
+    }
+}
